b663e05bba0e6f570e941b06dcb609d1ba0d092d
mheyns <m.heyns@imperial.ac.uk>
2024-07-10 02:11:22 +0100

    Update for ensemble prediction

--- src/DAGGER/Benchmark/Forecaster.py
+++ src/DAGGER/Benchmark/Forecaster.py
@@ -12,14 +12,16 @@ Will also Generate Weimer forecast at Supermag locations
 """
 
 
-def Forecaster(dataloader, model, dbe_mean, dbe_std, dbn_mean, dbn_std, target_index):
+def Forecaster(dataloader,models,dbe_mean,dbe_std,dbn_mean,dbn_std,target_index):
+    db_means = {'dbe': dbe_mean, 'dbn': dbn_mean}
+    db_stds = {'dbe': dbe_std, 'dbn': dbn_std}
     with torch.no_grad():
-        Predictions = {"dbe": [], "dbn": []}
-        Targets = {"dbe": [], "dbn": []}
-        dates = {"dbe": [], "dbn": []}
-        Coefficients = {"dbe": [], "dbn": []}
-        MLT_sup = {"dbe": [], "dbn": []}
-        Mcolat_sup = {"dbe": [], "dbn": []}
+        Predictions = {'dbe':[[] for _ in models],'dbn':[[] for _ in models]} 
+        Targets = {'dbe':[],'dbn':[]} 
+        dates = {'dbe':[],'dbn':[]} 
+        Coefficients = {'dbe':[[] for _ in models],'dbn':[[] for _ in models]} 
+        MLT_sup = {'dbe':[],'dbn':[]} 
+        Mcolat_sup = {'dbe':[],'dbn':[]} 
         for k in ["dbe", "dbn"]:
             for (
                 past_omni,
@@ -37,46 +39,45 @@ def Forecaster(dataloader, model, dbe_mean, dbe_std, dbn_mean, dbn_std, target_i
                 future_dates = future_dates.cuda()
                 mlt = mlt.cuda()
                 mcolat = mcolat.cuda()
-
-                _, _coeffsdb, db_pred = model(
-                    past_omni, past_supermag, mlt, mcolat, past_dates, future_dates
-                )
-
-                m = {"dbe": 0, "dbn": 1}
-                _coeffsdb = _coeffsdb[..., m[k]]
-                db_pred = db_pred[..., m[k]]
-
-                db_target = future_supermag[
-                    :, :, :, target_index[k]
-                ]  # *dbstd[k] + dbmean[k]
-                # unstandardize data
-                # db_pred = db_pred*dbstd[k] + dbmean[k]
-
-                Coefficients[k].append(_coeffsdb.detach().cpu().numpy())
-
-                Predictions[k].append(db_pred.detach().cpu().numpy())
+                
+                db_target = future_supermag[:,:,:,target_index[k]]
                 Targets[k].append(db_target.detach().cpu().numpy())
-
                 MLT_sup[k].append(mlt.cpu().numpy())
                 Mcolat_sup[k].append(mcolat.cpu().numpy())
+                dates[k].append(future_dates.cpu().numpy()) 
 
-                dates[k].append(future_dates.cpu().numpy())
-    All_times_coeff = {"dbe": [], "dbn": []}
-    Date_arr = {"dbe": [], "dbn": []}
-    MLT_sup_all = {"dbe": [], "dbn": []}
-    Mcolat_sup_all = {"dbe": [], "dbn": []}
+                for (i, model) in enumerate(models):
+                    _, _coeffsdb, db_pred = model(past_omni, past_supermag, mlt, mcolat, past_dates, future_dates)
+
+                    m = {'dbe': 0, 'dbn': 1}
+                    _coeffsdb = _coeffsdb[..., m[k]]
+                    db_pred = db_pred[..., m[k]]
+
+                    Coefficients[k][i].append(_coeffsdb.detach().cpu().numpy())
+                    Predictions[k][i].append(db_pred.detach().cpu().numpy())
+                    
+    All_times_coeff = {'dbe':[[] for _ in models],'dbn':[[] for _ in models]} 
+    Date_arr = {'dbe':[],'dbn':[]} 
+    MLT_sup_all = {'dbe':[],'dbn':[]} 
+    Mcolat_sup_all = {'dbe':[],'dbn':[]} 
+    variance = {}
+    
     for k in Predictions.keys():
-        Predictions[k] = np.concatenate(Predictions[k], axis=0)
-        Targets[k] = np.squeeze(np.concatenate(Targets[k], axis=0), axis=1)
-        All_times_coeff[k] = np.concatenate(Coefficients[k], axis=0)
-        Date_arr[k] = np.concatenate(dates[k], axis=0)
-        MLT_sup_all[k] = np.concatenate(MLT_sup[k], axis=0).squeeze(1)
-        Mcolat_sup_all[k] = np.concatenate(Mcolat_sup[k], axis=0).squeeze(1)
-    Predictions["dbe"] = Predictions["dbe"] * dbe_std + dbe_mean
-    Predictions["dbn"] = Predictions["dbn"] * dbn_std + dbn_mean
-    Targets["dbe"] = Targets["dbe"] * dbe_std + dbe_mean
-    Targets["dbn"] = Targets["dbn"] * dbn_std + dbn_mean
-    return Predictions, Targets, All_times_coeff, Date_arr, MLT_sup_all, Mcolat_sup_all
+        for i, model in enumerate(models):
+            Predictions[k][i] = np.concatenate(Predictions[k][i],axis=0)
+            All_times_coeff[k][i] = np.concatenate(Coefficients[k][i],axis=0)
+            Predictions[k][i] = Predictions[k][i]*db_stds[k] + db_means[k]
+
+        Targets[k] = np.squeeze(np.concatenate(Targets[k],axis=0),axis=1)
+        Targets[k] = Targets[k]*db_stds[k] + db_means[k]
+        Date_arr[k] = np.concatenate(dates[k],axis=0)
+        MLT_sup_all[k] = np.concatenate(MLT_sup[k],axis=0).squeeze(1)
+        Mcolat_sup_all[k] = np.concatenate(Mcolat_sup[k],axis=0).squeeze(1)
+        variance[k] = np.var(Predictions[k], axis = 0)
+        Predictions[k] = np.mean(Predictions[k], axis = 0)
+        All_times_coeff[k] = np.mean(All_times_coeff[k], axis = 0)
+
+    return Predictions,Targets,All_times_coeff,Date_arr,MLT_sup_all,Mcolat_sup_all, variance
 
 
 class Custom_interpolator:
