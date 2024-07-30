93e9feb143921e855c739c804dc28b0a1aeb8758
Chetraj Pandey <chetrajpandey@gmail.com>
2024-07-30 17:48:37 +0000

    linting

--- geocloak/sheath2024/create_tr_val_test_split.py
+++ geocloak/sheath2024/create_tr_val_test_split.py
@@ -1,13 +1,15 @@
+import os
+from io import StringIO
+
+import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-import numpy as np
-from io import StringIO 
-import os
+
 
 class DataProcessor:
     """
     A class to handle the processing of CSV data from a local directory.
-    
+
     Attributes:
         directory (str): The directory path where CSV files are located.
         column_names (list): List of column names from the merged DataFrame.
@@ -29,7 +31,7 @@ class DataProcessor:
         Returns:
             pd.DataFrame: A DataFrame containing the merged data from all CSV files.
         """
-        csv_files = [f for f in os.listdir(self.directory) if f.endswith('.csv')]
+        csv_files = [f for f in os.listdir(self.directory) if f.endswith(".csv")]
 
         dfs = []
         for file in csv_files:
@@ -40,14 +42,16 @@ class DataProcessor:
         merged_df = pd.concat(dfs)
         self.column_names = merged_df.columns.tolist()
         return merged_df
-    
+
     def create_splits(self, data, test_intervals, val_years_months):
         """
         Creates training, validation, and test sets from the given data based on specified intervals,
         ensuring that test and validation sets do not overlap.
         """
         initial_timestamp_col = self.column_names[0]
-        data[initial_timestamp_col] = pd.to_datetime(data[initial_timestamp_col], format='%Y-%m-%dT%H:%M:%S', errors='coerce')
+        data[initial_timestamp_col] = pd.to_datetime(
+            data[initial_timestamp_col], format="%Y-%m-%dT%H:%M:%S", errors="coerce"
+        )
 
         # Initialize boolean Series for test and validation indices
         test_indices = pd.Series([False] * len(data), index=data.index)
@@ -57,8 +61,11 @@ class DataProcessor:
         for start, end in test_intervals:
             start_date = pd.to_datetime(start)
             end_date = pd.to_datetime(end)
-            test_indices = test_indices | ((data[initial_timestamp_col] >= start_date) & (data[initial_timestamp_col] <= end_date))
-        
+            test_indices = test_indices | (
+                (data[initial_timestamp_col] >= start_date)
+                & (data[initial_timestamp_col] <= end_date)
+            )
+
         test_set = data[test_indices]
         print("Test set length:", len(test_set))
 
@@ -66,9 +73,11 @@ class DataProcessor:
         for key, value in val_years_months.items():
             if isinstance(value, tuple):
                 start_month, end_month = value
-                val_indices = val_indices | ((data[initial_timestamp_col].dt.year == key) & 
-                                            (data[initial_timestamp_col].dt.month >= start_month) & 
-                                            (data[initial_timestamp_col].dt.month <= end_month))
+                val_indices = val_indices | (
+                    (data[initial_timestamp_col].dt.year == key)
+                    & (data[initial_timestamp_col].dt.month >= start_month)
+                    & (data[initial_timestamp_col].dt.month <= end_month)
+                )
             else:
                 val_indices = val_indices | (data[initial_timestamp_col].dt.year == key)
 
@@ -95,21 +104,26 @@ class DataProcessor:
         """
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
-        
+
         # Define the paths for the files in the directory
-        train_path = os.path.join(output_directory, 'train_set.csv')
-        val_path = os.path.join(output_directory, 'val_set.csv')
-        test_path = os.path.join(output_directory, 'test_set.csv')
+        train_path = os.path.join(output_directory, "train_set.csv")
+        val_path = os.path.join(output_directory, "val_set.csv")
+        test_path = os.path.join(output_directory, "test_set.csv")
 
         # Save DataFrames to CSV files
         train_set.to_csv(train_path, index=False)
         val_set.to_csv(val_path, index=False)
         test_set.to_csv(test_path, index=False)
 
+
 def main():
     directory = "/home/chetrajpandey/data/formatted_data/sheath_train/"
     output_directory = "/home/chetrajpandey/data/formatted_data/sheath_splits"
-    test_intervals = [('2011-08-04', '2011-08-08'), ('2012-07-21', '2012-07-25'), ('2013-03-13', '2013-03-17')]
+    test_intervals = [
+        ("2011-08-04", "2011-08-08"),
+        ("2012-07-21", "2012-07-25"),
+        ("2013-03-13", "2013-03-17"),
+    ]
     val_years_months = {2011: (10, 12), 2012: (4, 6), 2013: (10, 12)}
     # val_years_months = {2011: (10, 12), 2012: (4, 6), 2013: None}  # Example of specifying whole year 2013
 
@@ -120,7 +134,9 @@ def main():
     data = processor.read_and_merge_csvs()
 
     # Create training, validation, and test splits
-    train_set, val_set, test_set = processor.create_splits(data, test_intervals, val_years_months)
+    train_set, val_set, test_set = processor.create_splits(
+        data, test_intervals, val_years_months
+    )
 
     # Save the splits to the local directory
     processor.save_splits(train_set, val_set, test_set, output_directory)
--- geocloak/sheath2024/create_train_test_validation_split.py
+++ geocloak/sheath2024/create_train_test_validation_split.py
@@ -1,13 +1,15 @@
+from io import StringIO
+
+import numpy as np
 import pandas as pd
 from google.cloud import storage
 from sklearn.preprocessing import StandardScaler
-import numpy as np
-from io import StringIO 
+
 
 class DataProcessor:
     """
     A class to handle the processing of CSV data from Google Cloud Storage.
-    
+
     Attributes:
         bucket_name (str): The name of the Google Cloud Storage bucket.
         directory (str): The directory path within the bucket where CSV files are located.
@@ -36,7 +38,7 @@ class DataProcessor:
         """
         bucket = self.client.get_bucket(self.bucket_name)
         blobs = bucket.list_blobs(prefix=self.directory)
-        csv_files = [blob.name for blob in blobs if blob.name.endswith('.csv')]
+        csv_files = [blob.name for blob in blobs if blob.name.endswith(".csv")]
 
         dfs = []
         for file in csv_files:
@@ -48,14 +50,16 @@ class DataProcessor:
         merged_df = pd.concat(dfs)
         self.column_names = merged_df.columns.tolist()
         return merged_df
-    
+
     def create_splits(self, data, test_intervals, val_years_months):
         """
         Creates training, validation, and test sets from the given data based on specified intervals,
         ensuring that test and validation sets do not overlap.
         """
         initial_timestamp_col = self.column_names[0]
-        data[initial_timestamp_col] = pd.to_datetime(data[initial_timestamp_col], format='%Y-%m-%dT%H:%M:%S', errors='coerce')
+        data[initial_timestamp_col] = pd.to_datetime(
+            data[initial_timestamp_col], format="%Y-%m-%dT%H:%M:%S", errors="coerce"
+        )
 
         # Initialize boolean Series for test and validation indices
         test_indices = pd.Series([False] * len(data), index=data.index)
@@ -65,8 +69,11 @@ class DataProcessor:
         for start, end in test_intervals:
             start_date = pd.to_datetime(start)
             end_date = pd.to_datetime(end)
-            test_indices = test_indices | ((data[initial_timestamp_col] >= start_date) & (data[initial_timestamp_col] <= end_date))
-        
+            test_indices = test_indices | (
+                (data[initial_timestamp_col] >= start_date)
+                & (data[initial_timestamp_col] <= end_date)
+            )
+
         test_set = data[test_indices]
         print("Test set length:", len(test_set))
 
@@ -74,9 +81,11 @@ class DataProcessor:
         for key, value in val_years_months.items():
             if isinstance(value, tuple):
                 start_month, end_month = value
-                val_indices = val_indices | ((data[initial_timestamp_col].dt.year == key) & 
-                                            (data[initial_timestamp_col].dt.month >= start_month) & 
-                                            (data[initial_timestamp_col].dt.month <= end_month))
+                val_indices = val_indices | (
+                    (data[initial_timestamp_col].dt.year == key)
+                    & (data[initial_timestamp_col].dt.month >= start_month)
+                    & (data[initial_timestamp_col].dt.month <= end_month)
+                )
             else:
                 val_indices = val_indices | (data[initial_timestamp_col].dt.year == key)
 
@@ -91,7 +100,6 @@ class DataProcessor:
 
         return train_set, val_set, test_set
 
-
     def save_splits(self, train_set, val_set, test_set, output_directory):
         """
         Uploads the training, validation, and test sets to CSV files in the specified GCS directory.
@@ -103,28 +111,32 @@ class DataProcessor:
             output_directory (str): The GCS directory where the CSV files will be saved.
         """
         bucket = self.client.get_bucket(self.bucket_name)
-        
+
         # Define the paths for the files in the bucket
-        train_path = f'{output_directory}/train_set.csv'
-        val_path = f'{output_directory}/val_set.csv'
-        test_path = f'{output_directory}/test_set.csv'
+        train_path = f"{output_directory}/train_set.csv"
+        val_path = f"{output_directory}/val_set.csv"
+        test_path = f"{output_directory}/test_set.csv"
 
         # Convert DataFrames to CSV strings
         train_csv = train_set.to_csv(index=False)
         val_csv = val_set.to_csv(index=False)
         test_csv = test_set.to_csv(index=False)
-        
+
         # Upload the CSV strings to GCS
-        bucket.blob(train_path).upload_from_string(train_csv, content_type='text/csv')
-        bucket.blob(val_path).upload_from_string(val_csv, content_type='text/csv')
-        bucket.blob(test_path).upload_from_string(test_csv, content_type='text/csv')
+        bucket.blob(train_path).upload_from_string(train_csv, content_type="text/csv")
+        bucket.blob(val_path).upload_from_string(val_csv, content_type="text/csv")
+        bucket.blob(test_path).upload_from_string(test_csv, content_type="text/csv")
 
 
 # Specifying the GCP bucket and folders
 bucket_name = "geocloak2024"
 directory = "formatted_data/sheath_train/"
 output_directory = "formatted_data/sheath_splits"
-test_intervals = [('2011-08-04', '2011-08-08'), ('2012-07-21', '2012-07-25'), ('2013-03-13', '2013-03-17')]
+test_intervals = [
+    ("2011-08-04", "2011-08-08"),
+    ("2012-07-21", "2012-07-25"),
+    ("2013-03-13", "2013-03-17"),
+]
 val_years_months = {2011: (10, 12), 2012: (4, 6), 2013: (10, 12)}
 # val_years_months = {2011: (10, 12), 2012: (4, 6), 2013: None}  # Example of specifying whole year 2013
 
@@ -135,7 +147,9 @@ processor = DataProcessor(bucket_name, directory)
 data = processor.read_and_merge_csvs()
 
 # Create training, validation, and test splits
-train_set, val_set, test_set = processor.create_splits(data, test_intervals, val_years_months)
+train_set, val_set, test_set = processor.create_splits(
+    data, test_intervals, val_years_months
+)
 
 # Save the splits to the local directory
 processor.save_splits(train_set, val_set, test_set, output_directory)
